6343b6e68846995f28ee18c2ff9c76733236c4a6
26VoukP <110846154+26VoukP@users.noreply.github.com>
2025-02-11 20:17:56 -0800

    Updated flag descriptions.

--- make_plots_from_meshes.py
+++ make_plots_from_meshes.py
@@ -3,9 +3,13 @@ import subprocess
 from absl import flags, app
 import mesh_stats_plotter
 
-_SCANNETPP_DATA_DIR = flags.DEFINE_string("scannetpp_data_dir", "./scanetpp_data", "Directory with the scanetpp meshes.")
+_SCANNETPP_DATA_DIR = flags.DEFINE_string("scannetpp_data_dir", "./scanetpp_data",
+                                          "Directory with the scanetpp data, in the structure originally downloaded"
+                                          " from the scannetpp dataset. This dir should contain subdirs with scene "
+                                          "names. ")
 _PRED_MESH_DIR = flags.DEFINE_string("pred_mesh_dir", "./pred_meshes",
-                                     "Directory with meshes from one mesh reconstructing algorithm.")
+                                     "Directory of .ply files of meshes from one reconstruction algorithm. The mesh"
+                                     " filenames should include the name of corresponding scene.")
 _OUTPUT_DIR = flags.DEFINE_string("output_dir", ".", "Directory in which to put folders for each scene with all data "
                                                      "relating to that scene.")
 _MSHCOMPARE_FILE = flags.DEFINE_string("mshcompare_file", "./mshcompare",
@@ -27,9 +31,9 @@ def main(argv):
         scene_output_dir = os.path.join(_OUTPUT_DIR.value, scene_name)
         scene_output_dirs.append(os.path.basename(scene_output_dir))
         os.makedirs(scene_output_dir, exist_ok=True)
+        # assumes pred_mesh will contain of scene name of scene in scannetpp dataset
         pred_meshes = [os.path.join(_PRED_MESH_DIR.value, f) for f in os.listdir(_PRED_MESH_DIR.value) if scene_name in f]
         for pred_mesh in pred_meshes:
-            # assumes pred_mesh will consist of scene name and name of reconstruction algorithm
             pred_basename = os.path.splitext(os.path.basename(pred_mesh))[0]
             pred_to_gt_stats = os.path.join(scene_output_dir, pred_basename + ".pred2gt.npy")
             gt_to_pred_stats = os.path.join(scene_output_dir, pred_basename + ".gt2pred.npy")
